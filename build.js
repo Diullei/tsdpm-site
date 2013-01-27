c10d68535568a1ba81d1e8d56079cd3faa0d06da
Diullei Gomes <diullei@gmail.com>
2013-01-27 17:50:30 -0200

    update minify script

--- build.js
+++ build.js
@@ -4,6 +4,22 @@ var jsp = require("uglify-js").parser;
 var pro = require("uglify-js").uglify;
 var util = require('util')
 
+function GUID() {
+    var S4 = function () {
+        return Math.floor(
+                Math.random() * 0x10000 /* 65536 */
+            ).toString(16);
+    };
+
+    return (
+            S4() + S4() + "-" +
+            S4() + "-" +
+            S4() + "-" +
+            S4() + "-" +
+            S4() + S4() + S4()
+        );
+}
+
 function min(content){
 	var orig_code = content;
 	var ast = jsp.parse(orig_code); // parse code and get the initial AST
@@ -20,10 +36,14 @@ var repo = fs.readFileSync('tmpl/repository.js', 'utf8');
 
 var script = repo + script_template;
 
-var html = index_template.replace('#####SCRIPT#####', min(script)).replace('#####SCRIPT-GOOGLE#####', min(google_script_template));
+var html = index_template
+	.replace('#####SCRIPT#####', min(script))
+	.replace('#####SCRIPT-GOOGLE#####', min(google_script_template))
+	.replace('####GUID####', GUID());
+	
 fs.writeFileSync('index.html', html, 'utf8');
 minify.optimize(['index.html']);
 
 var source_file = 'node_modules/minify/min/index.min.html';
 var source_file_content = fs.readFileSync(source_file, 'utf8');
-fs.writeFileSync('index.html', html, 'utf8');
+fs.writeFileSync('index.html', source_file_content, 'utf8');
